94dba57a9df6744cac09681502a4734d26d62315
Moquaddas Tanweer Waris <moquaddastanwees718@gmail.com>
2026-07-14 23:00:08 +0530

    Time: 12 ms (83.97%), Space: 25 MB (68.89%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public: 
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        vector<vector<string>> ans;
+        unordered_map<string, vector<string>> mp;
+
+        for(string str: strs){
+            string key = str;
+            sort(key.begin(), key.end()); //sort the value for key
+
+            mp[key].push_back(str);
+        }
+
+        for (const auto &x : mp) {
+            ans.push_back(x.second);
+        }
+        return ans;
+    }
+};
