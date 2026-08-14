20acb845ba71df5948cc6617020f303043c48100
Moquaddas Tanweer Waris <moquaddastanwees718@gmail.com>
2026-08-15 01:38:49 +0530

    Time: 3 ms (40.68%), Space: 12.6 MB (76.77%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -6,21 +6,16 @@ public:
         stack<int> st;
 
         // nums1 value -> index
-        for (int i = 0; i < nums1.size(); i++) {
+        for(int i=0; i<nums1.size(); i++){
             mp[nums1[i]] = i;
         }
 
-        for (int num : nums2) {
-
+        for (int num: nums2){
             while (!st.empty() && st.top() < num) {
                 int idx = mp[st.top()];
-
                 ans[idx] = num;
-
                 st.pop();
             }
-
-            // Only push elements that exist in nums1
             if (mp.find(num) != mp.end()) {
                 st.push(num);
             }
