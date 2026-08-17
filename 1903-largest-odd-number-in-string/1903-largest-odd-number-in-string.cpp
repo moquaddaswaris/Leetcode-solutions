916352a7841414a028a02f340cd6b13f26301d3c
Moquaddas Tanweer Waris <moquaddastanwees718@gmail.com>
2026-08-17 13:39:18 +0530

    Time: 0 ms (100%), Space: 17.8 MB (38.77%) - LeetHub

--- 1903-largest-odd-number-in-string/1903-largest-odd-number-in-string.cpp
+++ 1903-largest-odd-number-in-string/1903-largest-odd-number-in-string.cpp
@@ -2,11 +2,11 @@ class Solution {
 public:
     string largestOddNumber(string num) {
         string s = "";
-        while (!num.empty()) {
-            if ((num.back() - '0') % 2 != 0) {
+        while (!num.empty()){
+            if ((num.back()-'0')%2 != 0){
                 s = num;
                 break;
-            } else {
+            }else {
                 num.pop_back();
             }
         }
