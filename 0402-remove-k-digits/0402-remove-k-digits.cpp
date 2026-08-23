4de646d9d675a6c856c21a357531ce18e9336e81
Moquaddas Tanweer Waris <moquaddastanwees718@gmail.com>
2026-08-23 20:56:28 +0530

    Time: 4 ms (46.33%), Space: 11.1 MB (83.83%) - LeetHub

--- 0402-remove-k-digits/0402-remove-k-digits.cpp
+++ 0402-remove-k-digits/0402-remove-k-digits.cpp
@@ -3,21 +3,20 @@ public:
     string removeKdigits(string num, int k) {
         string st;
 
-        for (char ch : num) {
-            while (!st.empty() && k > 0 && st.back() > ch) {
+        for (char ch: num){
+            while(!st.empty() && k>0 && st.back()>ch){
                 st.pop_back();
                 k--;
             }
             st.push_back(ch);
         }
 
-        // Remove remaining digits from the end
-        while (k > 0) {
+        while(k>0){
             st.pop_back();
             k--;
         }
 
-        // Remove leading zeros
+        //remove leading zeros
         int i = 0;
         while (i < st.size() && st[i] == '0') {
             i++;
